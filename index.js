111ced4e2732df67e0d1098da267e21d89b94926
Sebastian <sebaorellana_2010@hotmail.com.ar>
2023-04-04 19:24:57 -0300

    04/04/2023 Added responsive Queries and animations

--- index.js
+++ index.js
@@ -1,19 +1,81 @@
-function animate_input(input_name, label_name, icon_name, top, left) {
+function clicked_input(input_name, label_name, icon_name, parent) {
     let input_user = document.getElementById(input_name);
     let label_user = document.getElementById(label_name);
     let icon_user = document.getElementById(icon_name);
+    let par = document.getElementById(parent);
     input_user.addEventListener("focus", () => {
-        label_user.style.top=top;
-        label_user.style.left=left;
+        label_user.style.top="0%";
+        label_user.style.left="0%";
         input_user.style.width="96.5%";
         input_user.style.visibility="visible";
         icon_user.style.opacity="0%";
+        label_user.classList.remove("anim");
+        switch (label_name) {
+            case "username_label":
+                par.removeEventListener("mouseout", de_animate_userlabel);
+                par.removeEventListener("mouseover", animate_userlabel);
+                break;
+            case "pass_label":
+                par.removeEventListener("mouseout", de_animate_passlabel);
+                par.removeEventListener("mouseover", animate_passlabel);
+                break;
+            case "mail_label":
+                par.removeEventListener("mouseout", de_animate_maillabel);
+                par.removeEventListener("mouseover", animate_maillabel);
+                break;
+        }
     })
 }
 
-animate_input("username", "username_label", "username_icon", "17.8rem", "49.5rem");
-animate_input("pass", "pass_label", "pass_icon", "22.8rem", "49.5rem");
-animate_input("mail", "mail_label", "mail_icon", "27.8rem", "49.5rem");
+function animate_userlabel() {
+    let elem = document.getElementById("username_label");
+    elem.classList.add("anim");
+}
+
+function de_animate_userlabel() {
+    let elem = document.getElementById("username_label");
+    elem.classList.remove("anim");
+}
+
+function animate_passlabel() {
+    let elem = document.getElementById("pass_label");
+    elem.classList.add("anim");
+}
+
+function de_animate_passlabel() {
+    let elem = document.getElementById("pass_label");
+    elem.classList.remove("anim");
+}
+
+function animate_maillabel() {
+    let elem = document.getElementById("mail_label");
+    elem.classList.add("anim");
+}
+
+function de_animate_maillabel() {
+    let elem = document.getElementById("mail_label");
+    elem.classList.remove("anim");
+}
+
+function animate_field(parent) {
+    let par = document.getElementById(parent);
+    switch (parent) {
+        case "username_list":
+            par.addEventListener("mouseover", animate_userlabel);
+            par.addEventListener("mouseout", de_animate_userlabel);
+            break;
+        case "pass_list":
+            par.addEventListener("mouseover", animate_passlabel);
+            par.addEventListener("mouseout", de_animate_passlabel);
+            console.log("work");
+            break;
+        case "mail_list":
+            par.addEventListener("mouseover", animate_maillabel);
+            par.addEventListener("mouseout", de_animate_maillabel);
+            break;
+    }
+
+}
 
 function submit_data() {
     button = document.getElementById("submit");
@@ -30,4 +92,12 @@ function submit_data() {
     })
 }
 
-submit_data()
+if(window.innerWidth > 1279) {
+    animate_field("username_list");
+    animate_field("pass_list");
+    animate_field("mail_list");
+    clicked_input("username", "username_label", "username_icon", "username_list");
+    clicked_input("pass", "pass_label", "pass_icon", "pass_list");
+    clicked_input("mail", "mail_label", "mail_icon", "mail_list");
+    submit_data()
+}
